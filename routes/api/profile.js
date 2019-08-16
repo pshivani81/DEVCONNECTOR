ba6c8f61e9ecd0a6bf6b81270f3219bb8cf479cd
shivani paidimarri <pshivani_75@yahoo.co.in>
2019-08-16 12:25:11 +0530

    check for profiles

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
-
-// @route GET api/profile
-// @desc Test route
-// @access Public
-router.get('/', (req, res) => res.send('profile route'));
+const auth = require('../../middleware/auth');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+// @route GET api/profile/me
+// @desc Get Current Users Profile
+// @access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.body.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
